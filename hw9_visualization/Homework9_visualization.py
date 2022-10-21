d44fcc84a3ac4fac172a2356d0effc79e65bc34f
Bilegt Baasandavaa <beegiimn@gmail.com>
2022-10-20 21:41:02 -0400

    The Homework9_visualization.py doesn't work

--- hw9_visualization/Homework9_visualization.py
+++ hw9_visualization/Homework9_visualization.py
@@ -1,12 +1,38 @@
 ## Homework 9: 
 
-
 ## Import
+from array import array
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-x = np.arange(0,4*np.pi,0.1)
-y = np.sin(x)
+
+## Ene graphic-iig harah
+var = pd.read_excel(r'C:\Users\beegi\OneDrive\Python\Homework_fall\hw9_visualization\wdi.xlsx', sheet_name='gdp')
+x   = list(var['year'])
+y   = list(var['USA'])
+#plt.ylim([0,5])
 plt.plot(x,y)
 plt.show()
-plt.savefig(hehe.png)
+
+## DON'T MIND
+df = pd.read_excel(r'C:\Users\beegi\OneDrive\Python\Homework_fall\hw9_visualization\wdi.xlsx')
+df[['year', 'USA']].plot.bar
+plt.show()
+
+var = pd.read_excel(r'C:\Users\beegi\OneDrive\Python\Homework_fall\hw9_visualization\wdi.xlsx', sheetname='debt')
+x   = list(var['year'])
+y   = list(var['USA'])
+plt.plot(x,y)
+plt.show()
+
+
+var = pd.read_excel(r'C:\Users\beegi\OneDrive\Python\Homework_fall\hw9_visualization\wdi.xlsx', sheetname='reserve')
+x   = list(var['year'])
+y   = list(var['USA'])
+plt.plot(x,y)
+plt.show()
+
+
+plt.title('yu ch yahve')
+plt.xlabel()
